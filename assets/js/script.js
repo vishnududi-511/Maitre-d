ab18e34af92f074a9da3f82afa8853c8cb569111
vishnududi-511 <dudivishnu4@gmail.com>
2024-04-19 11:08:36 +0530

    nav bar done

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let navitems = document.querySelector(".navitems");
+let menuicon = document.querySelector(".menuicon");
+
+menuicon.addEventListener("click", function () {
+    navitems.classList.toggle('show')
+});
