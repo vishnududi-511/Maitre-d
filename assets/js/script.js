ac397a337190889d661f52d58db2a80b3f460498
vishnududi-511 <dudivishnu4@gmail.com>
2024-04-24 10:28:26 +0530

    completed section 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,9 +44,41 @@ $(".slider").slick({
   ],
 });
 
-let navitems = document.querySelector(".navitems");
-let menuicon = document.querySelector(".menuicon");
+let toggle_box = document.querySelector(".toggle-ball");
+let pricebox = document.querySelectorAll(".price_box");
 
-menuicon.addEventListener("click", function () {
-  navitems.classList.toggle("show");
+pricebox.forEach((priceBox) => {
+  let tik_btn = priceBox.querySelector(".tik_checkbox");
+  let cross_btn = priceBox.querySelector(".cross_checkbox");
+  let second_box = priceBox.querySelector(".changing_box");
+  let third_box = priceBox.querySelector(".chnaging_box2");
+
+  toggle_box.addEventListener("click", function () {
+    toggle_box.classList.toggle("toggle-ball-right");
+    tik_btn.classList.toggle("tik");
+    cross_btn.classList.toggle("cross");
+    second_box.classList.toggle("cross");
+    third_box.classList.toggle("tik");
+  });
 });
+
+let toggleBall = document.querySelector(".toggle-ball");
+let subscriptionType = document.querySelector(".sup_month");
+let subscriptionamount = document.querySelector(".dollar_money");
+// Add click event listener to the toggle box
+
+  // Add click event listener to the toggle box
+  toggleBall.addEventListener("click", function () {
+    // Check the current subscription type
+    if (subscriptionType.innerHTML === "/Month") {
+      // If it's month, change it to yearly
+      subscriptionType.innerHTML = "/Yearly";
+      // Change the amount of the plan
+      subscriptionamount.innerHTML = "$999";
+    } else {
+      // If it's yearly, change it back to month
+      subscriptionType.innerHTML = "/Month";
+      // Revert the amount of the plan
+      subscriptionamount.innerHTML = "$449";
+    }
+  });
